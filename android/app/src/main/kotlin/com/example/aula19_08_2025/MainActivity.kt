6bd43029e37a7b33e65456bba82843da2f29a3e7
Bruno Torres <brunotorres.sl024@academico.ifsul.edu.br>
2025-08-19 19:50:05 -0300

    19082025

---
+++ android/app/src/main/kotlin/com/example/aula19_08_2025/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aula19_08_2025
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Exemplo Container',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Exemplo Container"),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+            color: Colors.amber[100],
+            borderRadius: BorderRadius.all(Radius.circular(50)),
+          ),
+          margin: EdgeInsets.all(30),
+          padding: EdgeInsets.all(50),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Conteúdo",
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
